198cd0883e4bedadfb53a97ac2959ac16459088c
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-08-03 23:10:32 +0300

    Working with exceptions

---
+++ Exceptions/Exceptions/ExceptionFilters.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public static class ExceptionFilters
+    {
+        public static bool IsDirectoryOrFileException(Exception ex)
+        {
+            var isDirOrFileException =
+                (
+                  (ex is ArgumentNullException argNullEx)
+                  && (
+                        string.Equals(argNullEx.ParamName, "directory", StringComparison.OrdinalIgnoreCase)
+                        || string.Equals(argNullEx.ParamName, "fileName", StringComparison.OrdinalIgnoreCase)
+                     )
+                )
+                || (ex is DirectoryNotFoundException)
+                || (ex is FileNotFoundException);
+
+            return isDirOrFileException;
+        }
+
+        public static bool IsFileContentReplaceException(Exception ex)
+        {
+            var isFilContentReplaceException =
+                (
+                  (ex is ArgumentNullException argNullEx)
+                  && (
+                        string.Equals(argNullEx.ParamName, "searchText", StringComparison.OrdinalIgnoreCase)
+                        || string.Equals(argNullEx.ParamName, "replaceWithText", StringComparison.OrdinalIgnoreCase)
+                     )
+                );
+
+            return isFilContentReplaceException;
+        }
+
+        public static bool LogException(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.StackTrace);
+
+            return false;
+        }
+    }
+}
---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+   {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ProcessFileWithRethrow();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error has occured");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                if (ex.InnerException is not null)
+                {
+                    Console.WriteLine("Inner exception:");
+                    Console.WriteLine(ex.InnerException.Message);
+                    Console.WriteLine(ex.InnerException.StackTrace);
+                }
+            }
+        }
+
+        private static void ProcessFile()
+        {
+            try
+            {
+                // TODO: Run in VS because DotNetFiddle doesn't handle console reads
+                Console.Write("Enter directory path=");
+                var directory = Console.ReadLine();
+
+                Console.Write("Enter file name=");
+                var fileName = Console.ReadLine();
+
+                Console.Write("Lookup text=");
+                var lookupText = Console.ReadLine();
+
+                Console.Write("Replace with text=");
+                var replaceWithText = Console.ReadLine();
+
+                FileProcessor.ReplaceText(directory, fileName, lookupText, replaceWithText);
+            }
+            catch (Exception exLog) when (ExceptionFilters.LogException(exLog))
+            {
+                Console.WriteLine("This should never be caught!");
+            }
+            catch (ArgumentNullException argEx) when (ExceptionFilters.IsDirectoryOrFileException(argEx))
+            {
+                Console.WriteLine("Directory or file exception!");
+            }
+            catch (ArgumentNullException argEx) when (ExceptionFilters.IsFileContentReplaceException(argEx))
+            {
+                Console.WriteLine("Content replace exception!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Other exception occured!");
+            }
+        }
+
+        private static void ProcessFileWithRethrow()
+        {
+            try
+            {
+                // TODO: Run in VS because DotNetFiddle doesn't handle console reads
+                Console.Write("Enter directory path=");
+                var directory = Console.ReadLine();
+
+                Console.Write("Enter file name=");
+                var fileName = Console.ReadLine();
+
+                Console.Write("Lookup text=");
+                var lookupText = Console.ReadLine();
+
+                Console.Write("Replace with text=");
+                var replaceWithText = Console.ReadLine();
+
+                FileProcessor.ReplaceText(directory, fileName, lookupText, replaceWithText);
+            }
+            catch (Exception ex)
+            {
+                /* cod care face ceva cu exceptia */
+                throw;
+            }
+        }
+    }
+}
